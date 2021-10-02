0143956eeb628e564a15d35d770cc0240c948b81
ashutoshkarwa18 <69465593+ashutoshkarwa18@users.noreply.github.com>
2021-10-02 16:29:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react"
 import ServicesTabs from "./components/Services/Tabs";
+import Dashboard from "./components/dashboard/Dashboard";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
 
 function App() {
   return (
+    
     <div className="App">
-     <ChakraProvider>
-     <ServicesTabs />
-    </ChakraProvider>
+      <Router>
+      <Switch>
+          <Route path="/Dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/ServicesTabs">
+            <ChakraProvider>
+            <ServicesTabs />
+            </ChakraProvider>
+          </Route>
+        </Switch>
+        </Router>
     </div>
   );
 }
